02f59751495cabab674b7cb97a7b31526f6d906f
Scrubbe-dev <scrubbe.dev@gmail.com>
2025-05-15 11:56:14 +0100

    Delete src directory

--- src/app/(pages)/(header)/products/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-const FeaturesPage = () => {
-   redirect("/features/api-as-a-service");
-};
-
-export default FeaturesPage;
-
-
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import HomePage from "./(pages)/homepage/page";
-
-
-export default function Home() {
-  return <HomePage />;
-}
