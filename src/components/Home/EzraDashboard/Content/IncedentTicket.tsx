80e9ed97a50a6507a5fe8032a2e9d5f29ecce96f
morakinyo <olamidemoraks@gmail.com>
2025-06-29 01:16:48 +0100

    feat: implement the new landing page

---
+++ src/components/Home/EzraDashboard/Content/IncedentTicket.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IncedentTicket = () => {
+  return (
+    <div className=" w-full h-full relative">
+      <img src="/natural_language_bg.png" className="" />
+    </div>
+  );
+};
+
+export default IncedentTicket;
